58448bfdb2684a1198c95743fe38eed2c3117056
Dylan Fontaine <me@dylf.dev>
2023-05-17 20:39:20 -0700

    start building a ball game

--- src/main.rs
+++ src/main.rs
@@ -1,93 +1,39 @@
 use bevy::prelude::*;
+use bevy::window::PrimaryWindow;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugin(PeoplePlugin)
+        .add_startup_system(spawn_player)
+        .add_startup_system(spawn_camera)
         .run();
 }
 
-pub struct PeoplePlugin;
-
-impl Plugin for PeoplePlugin {
-    fn build(&self, app: &mut App) {
-        app.add_startup_system(setup)
-            .add_system(print_names)
-            .add_system(people_with_jobs)
-            .add_system(people_unemployed)
-            .add_system(person_does_job);
-    }
-}
-
-pub fn setup(mut commands: Commands) {
-    commands.spawn((
-        Person {
-            name: "Dylan".to_string(),
-        },
-        Employed { job: Job::Doctor },
-    ));
-
-    commands.spawn((
-        Person {
-            name: "Bob".to_string(),
-        },
-        Employed {
-            job: Job::FireFighter,
-        },
-    ));
-    commands.spawn((Person {
-        name: "John".to_string(),
-    },));
-    commands.spawn((
-        Person {
-            name: "Jane".to_string(),
-        },
-        Employed { job: Job::Lawyer },
-    ));
-}
-
-pub fn print_names(person_query: Query<&Person>) {
-    for person in person_query.iter() {
-        println!("Name: {}", person.name);
-    }
-}
-
-pub fn people_with_jobs(person_query: Query<&Person, With<Employed>>) {
-    for person in person_query.iter() {
-        println!("{} has a job", person.name);
-    }
-}
-
-pub fn people_unemployed(person_query: Query<&Person, Without<Employed>>) {
-    for person in person_query.iter() {
-        println!("{} needs a job", person.name);
-    }
-}
-
-pub fn person_does_job(person_query: Query<(&Person, &Employed)>) {
-    for (person, employed) in person_query.iter() {
-        let job_name = match employed.job {
-            Job::Doctor => "Doctor",
-            Job::FireFighter => "Fire Fighter",
-            Job::Lawyer => "Lawyer",
-        };
-        println!("{} is a {}", person.name, job_name);
-    }
-}
-
 #[derive(Component)]
-pub struct Person {
-    pub name: String,
+pub struct Player;
+
+pub fn spawn_player(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+) {
+    let window = window_query.get_single().unwrap();
+
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+            texture: asset_server.load("sprites/ball_blue_large.png"),
+            ..default()
+        },
+        Player {},
+    ));
 }
 
-#[derive(Component)]
-pub struct Employed {
-    pub job: Job,
-}
+pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
+    let window = window_query.get_single().unwrap();
 
-#[derive(Debug)]
-pub enum Job {
-    Doctor,
-    FireFighter,
-    Lawyer,
+    commands.spawn(Camera2dBundle {
+        transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+        ..default()
+    });
 }
